ccc565191b025ac821eb6e903f6dffd6eeee972c
Sekai <ahmedx793@gmail.com>
2022-11-25 02:12:55 +0200

    ege

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
 export default NextAuth({
+  secret: process.env.SECRET!,
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID!,
