47b9bbc87639fb8bc33d3ffe1f0f48f54ae1c3e7
Sekai <ahmedx793@gmail.com>
2022-12-09 18:35:44 +0200

    favicon please??

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,22 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-	return (
-		<Html dir="rtl">
-			<Head />
-			<body id="root">
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+  return (
+    <Html dir="rtl">
+      <Head>
+        <title>Wolfland - عالم الخيال والابداع فى الرول بلاى</title>
+        <meta
+          name="description"
+          content="Wolfland - عالم الخيال والابداع فى الرول بلاى"
+        />
+        <link rel="icon" href="/favicon.ico" />
+
+        <meta property="og:image" content="/favicon.png"></meta>
+      </Head>
+      <body id="root">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
